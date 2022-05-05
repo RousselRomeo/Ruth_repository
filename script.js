b17caa59204302a546e8c768be8d462d4e65640f
RousselRomeo <rousselrommeo@yahoo.com>
2022-05-06 00:01:43 +0200

    added additional infos

--- script.js
+++ script.js
@@ -2,6 +2,32 @@ function home() {
   window.location.href = "index.html";
 }
 
+function renewableEnergy() {
+  window.location.href = "renewableEnergy.html";
+}
+function commercial() {
+  window.location.href = "commercial.html";
+}
+
+function audioVisual() {
+  window.location.href = "audioVisual.html";
+}
+
+function dataCenters() {
+  window.location.href = "dataCenters.html";
+}
+
+function entertainment() {
+  window.location.href = "entertainment.html";
+}
+
+function serviceAndMaintenance() {
+  window.location.href = "serviceAndMaintenance.html";
+}
+
+function healthcare() {
+  window.location.href = "healthcare.html";
+}
 function aboutUs() {
   window.location.href = "index3.html";
 }
